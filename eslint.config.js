dcd05ddb4949d1e0ac2d05fecd197842c2500f17
Dominik Kowalczyk <dominik.kowalczyk17@gmail.com>
2024-08-26 14:44:48 +0200

    connect db, handle POST

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,22 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import airbnbBase from "eslint-config-airbnb-base";
+import importPlugin from "eslint-plugin-import";
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node, // Node.js globals
+      },
+    },
+  },
+  pluginJs.configs.recommended, // ESLint's recommended config
+  airbnbBase, // Airbnb's base config
+  {
+    plugins: {
+      import: importPlugin, // Needed for Airbnb config
+    },
+  },
 ];
